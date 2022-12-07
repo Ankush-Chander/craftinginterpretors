1777f42a806aad703a3aa14f32779676ea0b96e6
Ankush-Chander <ankush.watchtower@gmail.com>
2022-12-07 19:14:58 +0530

    add code in Lox file.

--- java/com/craftinginterpretors/lox/Lox.java
+++ java/com/craftinginterpretors/lox/Lox.java
@@ -1,1 +1,28 @@
+package lox;
+//java/com/craftinginterpretors
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Lox {
+    public static void main(String[] args) throws IOException{
+        if (args.length >1){
+            System.out.println("Usage: jlox [script]");
+        }
+//        elif(args.length==1){
+//            runFile(args[0]);
+//        }else{
+//            runPrompt();
+//        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+//        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        
+    }
+}
